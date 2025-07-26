e338766382bc2fd66e5339b0973da6e3e263029d
Harish <27229309+Harishhari0525@users.noreply.github.com>
2025-07-26 13:33:59 +0530

    Update build.gradle.kts

--- TamilMVProvider/build.gradle.kts
+++ TamilMVProvider/build.gradle.kts
@@ -8,7 +8,7 @@ version = 1
 
 cloudstream {
     description = "Telugu, Tamil, Malayalam, Kannada, Hindi & Web Series torrents from 1TamilMV.se"
-    authors = listOf("YouKnowWho")
+    authors = listOf("Yo")
     language = "en"
     status = 1
     tvTypes = listOf("Movie", "Series")
